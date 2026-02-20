58494b8c52dc987c37147eb7832b5e63d88cfe97
Owner <sylviahutzler21@gmail.com>
2026-02-20 07:54:09 -0700

    added documentation page

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,7 +1,15 @@
+import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
+
 function Contact() {
+    const navigate = useNavigate();
     return (
         <div>
-            Contact
+            For bugs email sylviahutzler21@gmail.com or kayleewood66@gmail.com
+            For documentation visit github or click
+            <Button variant="text" sx={{color: '#8B9D83'}} onClick={() => navigate('/documentation')}>
+                here
+            </Button>
         </div>
     )
 }
