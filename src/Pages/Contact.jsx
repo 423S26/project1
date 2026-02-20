efd4ef5482d424a4c80e3a8e624ab26fc3e75996
kaylee <kayleewood66@gmail.com>
2026-02-20 12:03:10 -0700

    Updated contact page with links and user documentation

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -6,7 +6,13 @@ function Contact() {
     return (
         <div>
             For bugs email sylviahutzler21@gmail.com or kayleewood66@gmail.com
-            For documentation visit github or click
+            For documentation visit <a href="https://github.com/423S26/project1/blob/main/README.md"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      style={{
+                        color: '#8B9D83',
+                        fontWeight: 500,
+                      }}> GitHub</a> or click
             <Button variant="text" sx={{color: '#8B9D83'}} onClick={() => navigate('/documentation')}>
                 here
             </Button>
