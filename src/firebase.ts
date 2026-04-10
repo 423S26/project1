8b751c85634b6e757ae6200c3cae7ac270b7bf97
Owner <sylviahutzler21@gmail.com>
2026-04-10 16:11:21 -0600

    fix api security

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,13 +4,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import{ getFirestore} from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCkpYXhe6oBbc06PS8dpdgXEGnGCKWWdGY",
-  authDomain: "project1-2c236.firebaseapp.com",
-  projectId: "project1-2c236",
-  storageBucket: "project1-2c236.firebasestorage.app",
-  messagingSenderId: "608482107880",
-  appId: "1:608482107880:web:439d86a8abcedc97d9445d",
-  measurementId: "G-TGVHRHRWXX"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
